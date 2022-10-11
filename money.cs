7ae6b8ec4cf9e9a6e955303929b33bb6ecbac3bd
CauaCriz <90722412+CauaCriz@users.noreply.github.com>
2022-10-11 20:10:05 -0300

    update money.cs

---
+++ money.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class money : MonoBehaviour
+{
+    public int MoneyPass = 0;
+    
+    //Use this initialization
+    void Start()
+    {
+        
+    }
+    
+    //Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTrigerEnter(Collider collision)
+    {
+        if(ComparTag("Money"))
+        {
+            MoneyPass += 5;
+            Debug.Log("Voce agr tem: " + MoneyPass + "de dinheiro");
+            Destroy(collision);
+        }
+    }
+}
